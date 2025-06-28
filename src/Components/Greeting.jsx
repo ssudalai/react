c8c1affe9d46db9c62936687cf81af87a835b16c
ssudalai <sudalai3192@gmail.com>
2025-06-28 12:24:08 +0530

    js in html

--- src/Components/Greeting.jsx
+++ src/Components/Greeting.jsx
@@ -1,7 +1,11 @@
 function Greeting(){
+  let num1=3
+  let num2=4
+  
   return <>
   <h1>Hello, world !</h1>
     <p>this is sudalai</p>
+    <p>The sum of {num1} and {num2} is {num1+num2}</p>
   </>
 }
 
